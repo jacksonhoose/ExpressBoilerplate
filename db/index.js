98fb335470c51221a8286581241f86d41e46a36d
Jackson Hoose <jacksonhoose@gmail.com>
2015-01-15 15:10:44 -0800

    moved mongoose connection

---
+++ db/index.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('MongoDB connection is open.');
+});
+
+module.exports = db;
