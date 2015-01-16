c4fbc8d7b71ddf89a506f0e793d0d91831ff1d33
Jackson Hoose <jacksonhoose@gmail.com>
2015-01-15 16:07:21 -0800

    added gulpfile and folder structure

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var karma = require('karma').server;
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var image = require('gulp-image');
+var compass = require('gulp-compass');
+var minifyCSS = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var rename = require('gulp-rename');
+
+var paths = {
+  src: {
+    bower: './app/src/bower_components',
+    js: './app/src/js',
+    scss: './app/src/scss',
+    img: './app/src/img'
+  },
+  dist: {
+    js: './app/dist/js',
+    css: './app/dist/css',
+    img: './app/dist/img'
+  },
+  karmaConf: __dirname + '/karma.conf.js'
+};
+
+var handleError = function(err) {
+  console.log(err.toString());
+  this.emit('end');
+};
+
+gulp.task('browserify', function(){
+  return gulp.src(paths.src.js + '/app.js')
+    .pipe(browserify())
+    .pipe(rename('app.bundled.js'))
+    .pipe(gulp.dest(paths.src.js));
+});
+
+gulp.task('javascript', function() {
+  return gulp.src([
+      paths.src.js + '/app.js'
+    ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify({
+      beautify: true
+    }))
+    .on('error', handleError)
+    .pipe(gulp.dest(paths.dist.js));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(paths.src.js + '/app.bundled.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('image', function() {
+  return gulp.src(paths.src.img + '/**/*')
+    .pipe(image())
+    .on('error', handleError)
+    .pipe(gulp.dest(paths.dist.img));
+});
+
+gulp.task('compass', function() {
+  return gulp.src(paths.src.scss + '/app.scss')
+    .pipe(compass({
+      css: paths.dist.css,
+      sass: paths.src.scss,
+      image: paths.dist.img,
+      require: ['breakpoint']
+    }))
+    .on('error', handleError)
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.dist.css));
+});
+
+gulp.task('test', function(done){
+  karma.start({
+    configFile: paths.karmaConf,
+    singleRun: true
+  }, done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.src.img + '/**/*', ['image']);
+  gulp.watch(paths.src.scss + '/**/*.scss', ['compass']);
+  gulp.watch(paths.src.js + '/**/*.js', ['lint', 'javascript']);
+});
+
+gulp.task('default', ['compass', 'image', 'lint', 'javascript', 'watch']);
